8919d65e3411b42f8acf2b975dca25ebdf07bf36
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2023-08-16 21:34:51 +0530

    Create 16_3Sum Closest.java

---
+++ 16_3Sum Closest.java
@@ -0,0 +1,55 @@
+// Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+// Return the sum of the three integers.
+
+// You may assume that each input would have exactly one solution.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+// Example 2:
+
+// Input: nums = [0,0,0], target = 1
+// Output: 0
+// Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 500
+// -1000 <= nums[i] <= 1000
+// -104 <= target <= 104
+
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum with the sum of the first three elements
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum; // Update closest sum if the current sum is closer to the target
+                }
+
+                if (sum < target) {
+                    j++; // Increment j to increase the sum
+                } else {
+                    k--; // Decrement k to decrease the sum
+                }
+            }
+        }
+
+        return closestSum;
+    }
+}
