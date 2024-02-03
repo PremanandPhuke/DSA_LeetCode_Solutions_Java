104083c2463474565b1da8398b9edd3b2def2306
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2024-02-03 20:23:21 +0530

    Added different approaches 🔥🔥

--- 217_Contains Duplicate.java
+++ 217_Contains Duplicate.java
@@ -19,8 +19,22 @@
 // 1 <= nums.length <= 105
 // -109 <= nums[i] <= 109
 
+//Approach 1 : Brute Force
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
 
 
+//Approach 2 : Sorting
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums); 
@@ -28,6 +42,19 @@ class Solution {
                 if (nums[i] == nums[i+1]) {
                     return true;
                 }
+        }
+        return false;
+    }
+}
+
+//Approach 3 : HashSet
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
         }
         return false;
     }
