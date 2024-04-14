1bc14975751c7031e3a123b778fb6d5853023531
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2024-04-14 16:58:12 +0530

    Added one more solution

--- 27_Remove Element.java
+++ 27_Remove Element.java
@@ -45,6 +45,7 @@
 // 0 <= nums[i] <= 50
 // 0 <= val <= 100
 
+//Solution 1
 class Solution {
     public int removeElement(int[] nums, int val) {
         
@@ -62,5 +63,19 @@ class Solution {
         }
 
         return i;
+    }
+}
+
+//Solution 2
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    int k = 0; // Number of elements not equal to val
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i]; // Move non-val elements to the front
+            k++; // Increment k for each non-val element
+        }
+    }
+    return k;
     }
 }
