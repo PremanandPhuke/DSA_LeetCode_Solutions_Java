47ddc25305b754823937b7738226045293a176d7
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2024-04-25 10:13:25 +0530

    Multiple Solutions Added 🔥🔥

--- 238_Product of Array Except Self.java
+++ 238_Product of Array Except Self.java
@@ -22,7 +22,7 @@
 // -30 <= nums[i] <= 30
 // The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
   
-
+//Solution 1
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
@@ -39,5 +39,74 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+//Solution 2
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+        
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
+        }
+        return ans;
+    }
+}
+
+
+//Solution 3
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+        
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
+        }
+        return ans;
+    }
+}
+
+//Solution 5
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, 1);
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        return ans;
     }
 }
