ab8f823aa3a9b72faa3ba41cc8950b4ae3d3b149
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2024-04-30 20:38:36 +0530

    Create 383_Ransom Note.java

---
+++ 383_Ransom Note.java
@@ -0,0 +1,55 @@
+
+// Solved
+// Easy
+// Topics
+// Companies
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= ransomNote.length, magazine.length <= 105
+// ransomNote and magazine consist of lowercase English letters.
+
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character, Integer> list= new HashMap<>();
+
+        for(char c : ransomNote.toCharArray()){
+            list.put(c, list.getOrDefault(c, 0) +1);
+        }
+
+        for(char c : magazine.toCharArray()){
+            if(list.containsKey(c)){
+                list.put(c, list.getOrDefault(c, 0)-1 );
+            }
+        }
+
+        for(int val: list.values()){
+            if(val>0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
