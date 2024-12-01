4ab23272613c56ef80654d8095dc9822e96dc5a8
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2024-12-02 00:05:08 +0530

    Add multiple Solutions 🔥💪

--- 189_Rotate Array.java
+++ 189_Rotate Array.java
@@ -21,9 +21,69 @@
 // 1 <= nums.length <= 105
 // -231 <= nums[i] <= 231 - 1
 // 0 <= k <= 105
- 
 
 
+// Solution 1: Using Extras Array
+class Solution{
+ public void rotate(int[] nums, int k) {
+    int n = nums.length;
+    k %= n;
+    int[] temp = new int[n];
+    for (int i = 0; i < n; i++) {
+        temp[(i + k) % n] = nums[i];
+    }
+    System.arraycopy(temp, 0, nums, 0, n);
+}
+// or
+
+ class Solution{
+  public void rotate(int[] nums, int k) {
+
+        int n=nums.length;
+        k=k%n;
+
+        int arr[]= new int[n];
+        int j=0;
+
+        for(int i=n-k; i<n; i++){
+            
+            arr[j++]=nums[i];
+        }
+
+        for(int i=0; i<n-k;i++){
+            arr[j++]=nums[i];
+        }
+
+        int y=0;
+        for(int i:arr){
+            nums[y++]=i;
+        }
+    }
+ }
+
+ //Solution 2:  Reverse Method 
+ class Solution{
+  public void rotate(int[] nums, int k) {
+    int n = nums.length;
+    k %= n;
+
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
+}
+
+private void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+}
+
+// Solution 3: Cyclic Replacement 
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
