9a57d92e162719c483539e4bba8b8a2454c9f2e1
Premanand Dilip Phuke <104299540+PremanandPhuke@users.noreply.github.com>
2026-05-28 11:09:07 +0530

    Add iterative Fibonacci solution to the class

--- 509_Fibonacci Number.java
+++ 509_Fibonacci Number.java
@@ -56,3 +56,27 @@ class Solution {
        return arr[n];
     }
 }
+
+
+///anothe dp solution
+
+class Solution {
+    public int fib(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+}
